0c2ee2530d70c3ba1f91c1a721d0ecc039fb880a
Tsukiyama-do <yuichi.sr.hirose@gmail.com>
2020-01-04 17:29:57 +0900

    fix

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import router from './router'
 import BootstrapVue from 'bootstrap-vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
- 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
